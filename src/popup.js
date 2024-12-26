2284860a4bacf5346eb134d409a6cb9c28db9daa
TienThinh-fe <tienthinhhoang317@gmail.com>
2024-12-27 00:37:57 +0700

    feat: use input and add some style

--- src/popup.js
+++ src/popup.js
@@ -2,10 +2,15 @@
 
 import './popup.css';
 
-const captureText = document.getElementById('capture-text');
+const CONVERT_GMT_DEFAULT_TEXT = 'Convert to GMT';
+const CONVERT_LOCAL_TIME_DEFAULT_TEXT = 'Convert to Local Time';
+const RELATIVE_TIME_DEFAULT_TEXT = 'Relative Time';
+
+const captureTextInput = document.getElementById('capture-text');
 const convertGmt = document.getElementById('convert-gmt');
 const convertLocalTime = document.getElementById('convert-local-time');
 const relativeTime = document.getElementById('relative-time');
+const clearBtn = document.getElementById('clear-btn');
 
 const getGmtFromUnix = (unix) => {
   const date = new Date(unix * 1000);
@@ -18,14 +23,41 @@ const getLocalTimeFromUnix = (unix) => {
 };
 
 const getRelativeTimeFromUnix = (unix) => {
-  const date = new Date(unix * 1000);
-  return date.toLocaleString();
+  const current = Math.floor(new Date().getTime() / 1000);
+
+  const diff = current - unix;
+  let output = '';
+
+  if (diff < 60) {
+    output = 'just now';
+  } else if (diff < 3600) {
+    output = `${Math.floor(diff / 60)} minutes ago`;
+  } else if (diff < 86400) {
+    output = `${Math.floor(diff / 3600)} hours ago`;
+  } else {
+    output = `${Math.floor(diff / 86400)} days ago`;
+  }
+
+  return output;
 };
+
+captureTextInput.addEventListener('change', (event) => {
+  const selectedText = event.target.value;
+  chrome.storage.local.set({ selectedText });
+});
+
+clearBtn.addEventListener('click', () => {
+  chrome.storage.local.remove('selectedText');
+  captureTextInput.value = '';
+  convertGmt.textContent = CONVERT_GMT_DEFAULT_TEXT;
+  convertLocalTime.textContent = CONVERT_LOCAL_TIME_DEFAULT_TEXT;
+  relativeTime.textContent = RELATIVE_TIME_DEFAULT_TEXT;
+});
 
 // Retrieve the stored selected text when the popup is opened
 chrome.storage.local.get('selectedText', (result) => {
   if (result.selectedText) {
-    captureText.textContent = result.selectedText;
+    captureTextInput.value = result.selectedText;
 
     const unix = parseInt(result.selectedText);
     convertGmt.textContent = getGmtFromUnix(unix);
@@ -38,12 +70,14 @@ chrome.storage.local.get('selectedText', (result) => {
 chrome.storage.onChanged.addListener((changes, namespace) => {
   for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
     if (key === 'selectedText') {
-      captureText.textContent = newValue;
+      if (newValue) {
+        captureTextInput.value = newValue;
 
-      const unix = parseInt(newValue);
-      convertGmt.textContent = getGmtFromUnix(unix);
-      convertLocalTime.textContent = getLocalTimeFromUnix(unix);
-      relativeTime.textContent = getRelativeTimeFromUnix(unix);
+        const unix = parseInt(newValue);
+        convertGmt.textContent = getGmtFromUnix(unix);
+        convertLocalTime.textContent = getLocalTimeFromUnix(unix);
+        relativeTime.textContent = getRelativeTimeFromUnix(unix);
+      }
     }
   }
 });
